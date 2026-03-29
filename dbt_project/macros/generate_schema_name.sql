5ac5447b947610d24e21ad9a93b196f5c6bf9d4e
NanoCarbon <edward.w.lin@gmail.com>
2026-03-29 11:59:44 -0400

    Fix schema macro

--- dbt_project/macros/generate_schema_name.sql
+++ dbt_project/macros/generate_schema_name.sql
@@ -1,11 +1,11 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
     {%- if custom_schema_name is none -%}
-        {{ default_schema }}
+        {{ target.schema }}
     {%- else -%}
         {%- if target.name == 'prod' -%}
             {{ custom_schema_name | trim }}
         {%- else -%}
-            dev_{{ custom_schema_name | trim }}
+            {{ target.schema }}_{{ custom_schema_name | trim }}
         {%- endif -%}
     {%- endif -%}
 {%- endmacro %}
