fc18e603f792183b43be8bb6dd754696092d4332
Edward Lin <edward.w.lin@gmail.com>
2026-03-29 01:12:01 +0000

    -m nanosec bug fix

--- dbt_project/models/staging/stg_prices.sql
+++ dbt_project/models/staging/stg_prices.sql
@@ -5,13 +5,13 @@ with source as (
 renamed as (
     select
         ticker,
-        to_date(dateadd(second, date / 1000, '1970-01-01'))  as price_date,
+        to_date(dateadd(second, date / 1000000000, '1970-01-01'))  as price_date,
         cast(open as float)                                   as open_price,
         cast(high as float)                                   as high_price,
         cast(low as float)                                    as low_price,
         cast(close as float)                                  as close_price,
         cast(volume as bigint)                                as volume,
-        to_timestamp(extracted_at / 1000)                     as extracted_at
+        to_timestamp(extracted_at / 1000000000)                     as extracted_at
     from source
     where close is not null
       and volume > 0
